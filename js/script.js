53c209d9238580638cbfac932b2a961d4d5182b2
TristanKhadka11 <145160199+TristanKhadka11@users.noreply.github.com>
2024-02-26 15:58:05 -0500

    P1-3 APP v4

--- js/script.js
+++ js/script.js
@@ -1,17 +1,38 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const heartButton = document.getElementById('heartButton');
-    let isHeartFilled = false;
-  
-    heartButton.addEventListener('click', function () {
-      const heartPath = heartButton.querySelector('svg path');
-      isHeartFilled = !isHeartFilled;
-  
-      if (isHeartFilled) {
-        heartPath.setAttribute('fill', 'pink');
-      } else {
-        heartPath.setAttribute('fill', 'currentColor');
-      }
-    });
+  const heartButton = document.getElementById('heartButton');
+  let isHeartFilled = false;
+
+  heartButton.addEventListener('click', function () {
+    const heartPath = heartButton.querySelector('svg path');
+    isHeartFilled = !isHeartFilled;
+
+    if (isHeartFilled) {
+      heartPath.setAttribute('fill', 'pink');
+    } else {
+      heartPath.setAttribute('fill', 'currentColor');
+    }
   });
-  
-  
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  var swiper = new Swiper('.swiper-filters', {
+    slidesPerView: 'auto',
+    spaceBetween: 10,
+    freeMode: true,
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+
+  var swiperFeaturedArtworks = new Swiper('.swiper-featured-artworks', {
+    slidesPerView: 'auto',
+    centeredSlides: false,
+    spaceBetween: 10,
+    freeMode: true,
+  });
+
+
+});
+
+
+
